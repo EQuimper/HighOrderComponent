e4036cdcdea00fbb2a1ecb407ee0fa992b94a101
EQuimper <quimperemanuel@gmail.com>
2016-05-25 15:10:59 -0600

    Finished the HOC section

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import {
 	browserHistory
 } from 'react-router';
 
+import requireAuth from './components/Require_Auth';
+
 import App from './components/app';
 import Resources from './components/Resources';
 import reducers from './reducers';
@@ -21,7 +23,7 @@ render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
 		<Router history={browserHistory}>
 			<Route path="/" component={App}>
-				<Route path="resources" component={Resources} />
+				<Route path="resources" component={requireAuth(Resources)} />
 			</Route>
 		</Router>
 	</Provider>
